917976b401633299587d7c3666516f0768a6191a
Harith Azmi <harith2312@gmail.com>
2022-07-13 19:35:15 +0800

    Login Page
    - added sanitize function for name and password
    - added script to alert user upon login whether success or failure
    - added see password for easier checks

--- login.php
+++ login.php
@@ -1,35 +1,62 @@
 <?php
 include "header.php";
 require "connect.php";
-session_start();
 
 if (isset($_POST['loginProcess'])) {
-	$librarianid = $_POST['librarianid'];
-	$password = $_POST['password'];
-	$query = "SELECT *  FROM LIBRARIAN WHERE LIBRARIAN_ID = '$librarianid' AND LIBRARIAN_PASS = '$password'";
-	//query is sent to the db to fetch row id.
-	$stid = oci_parse($connect, $query);
 
-	oci_execute($stid);
-	$row = oci_fetch_array($stid);
+	function validate($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	$librarian_name = validate($_POST['librarian_name']);
+	$password = validate($_POST['password']);
+
+	$query = "SELECT * FROM LIBRARIAN WHERE LIBRARIAN_NAME = '$librarian_name' AND LIBRARIAN_PASS = '$password'";
+
+	$result = oci_parse($connect, $query);
+	oci_execute($result);
+	$row = oci_fetch_array($result);
 
 	if (is_array($row)) {
-		$_SESSION['LIBRARIAN_ID'] = $row['LIBRARIAN_ID'];
-		header("Location: index.php");
+		$_SESSION['LIBRARIAN_PASS'] = $row['LIBRARIAN_PASS'];
+		?>
+		<script>
+			alert("Login Successful");
+			window.location.href = "index.php";
+		</script>
+		<?php
 	} else {
-		echo "Invalid Librarian ID or Password!";
+		echo "<script>alert('Invalid username or password');</script>";
 	}
 }
 ?>
 
-<?php include 'header.php'; ?>
 <form align="center" method="POST">
 
-    <br><img src="img/library.jpg" alt="Logo" width="445" height="250" align="center"><br>
-    <input type="text" name="librarianid" placeholder="Librarian ID" ><br/><br/>
-    <input type="password" name="password" placeholder="Password"><br/><br/>
-    <button type="submit" name="loginProcess">Log In</button>
-
+	<br>
+	<img src="img/library.jpg" alt="Logo" width="445" height="250">
+	<br>
+	<input type="text" name="librarian_name" placeholder="Librarian Name"><br /><br />
+	<input type="password" name="password" id="librarian-password" placeholder="Password"><br>
+	<div class="form-check form-check-inline my-2">
+		<input class="form-check-input" onclick="seePassword()" type="checkbox" id="password_input" value="option1">
+		<label class="form-check-label" for="inlineCheckbox1">See Password</label>
+	</div>
+	<br>
+	<button type="submit" name="loginProcess" class="btn btn-primary">Login</button>
 </form>
+<script>
+	function seePassword() {
+		var x = document.getElementById('librarian-password')
+		if (x.type === 'password') {
+			x.type = 'text'
+		} else {
+			x.type = 'password'
+		}
+	}
+</script>
 
 <?php include 'footer.php'; ?>
