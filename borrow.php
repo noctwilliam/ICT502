d2e2485e3a3af23b75b6b354ad5d94c21a723298
Harith Azmi <harith2312@gmail.com>
2022-07-14 04:07:13 +0800

    Borrow Index
    - rid of navbar
    - display book_title, user_name, and librarian_id

--- borrow.php
+++ borrow.php
@@ -1,5 +1,4 @@
 <?php
-
 	require "connect.php";
 	$query = "SELECT * FROM BORROWED";
 	$result = oci_parse($connect, $query);
@@ -8,44 +7,19 @@
 
 <?php include "header.php"; ?>
 	<section id="table">
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">View User</a>
-	</li>
-	<li class="nav-item">
-        <a class="nav-link" href="borrow.php">Borrowed Books</a>
-	</li><li class="nav-item">
-        <a class="nav-link" href="#">Report</a>
-	</li>
-	
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Log Out</a>
-	</li>
-    </ul>
-  </div>
-</nav>
 		<div class="container">
-		<div class="align-center my-5">
-			<form class="row" action="searchborrow.php" method="POST">
-				<div class="col-auto"><input type="text" class="form-control" id="search" name="searchvalue" placeholder="Search"></div>
-				<div class="col-auto"><button type="submit" name="search" class="btn btn-primary">Search</button></div>
-			</form>
-		</div>
+			<div class="align-center my-5">
+				<form class="row" action="searchborrow.php" method="POST">
+					<div class="col-auto"><input type="text" class="form-control" id="search" name="searchvalue" placeholder="Search"></div>
+					<div class="col-auto"><button type="submit" name="search" class="btn btn-primary">Search</button></div>
+				</form>
+			</div>
 			<table class="table table-striped">
 				<thead>
 					<tr>
+						<th>BOOK ISBN</th>
                         <th>USER ID</th>
-						<th>BOOK ID</th>
+
 						<th>LIBRARIAN ID</th>
 						<th>RESERVE</th>
 						<th>RETURN</th>
@@ -56,15 +30,36 @@
 				<tbody class="table-group-divider">
 					<?php while ($row = oci_fetch_array($result)) { ?>
 						<tr>
-                            <td><?php echo $row['BOOK_ID']; ?></td>
-							<td><?php echo $row['USER_ID']; ?></td>
-							<td><?php echo $row['LIBRARIAN_ID']; ?></td>
+                            <td><?php
+								$book_row = $row['BOOK_ISBN'];
+								$book_sql = "SELECT * FROM BOOK WHERE BOOK_ISBN = '$book_row'";
+								$book_result = oci_parse($connect, $book_sql);
+								oci_execute($book_result);
+								$book_row = oci_fetch_array($book_result);
+								echo $book_row['BOOK_TITLE'];
+							?></td>
+							<td><?php
+								$user_row = $row['USER_ID'];
+								$user_sql = "SELECT * FROM USERS WHERE USER_ID = '$user_row'";
+								$user_result = oci_parse($connect, $user_sql);
+								oci_execute($user_result);
+								$user_data = oci_fetch_array($user_result);
+								echo $user_data['USER_NAME'];
+							?></td>
+							<td><?php 
+								$librarian_row = $row['LIBRARIAN_ID'];
+								$librarian_sql = "SELECT * FROM LIBRARIAN WHERE LIBRARIAN_ID = '$librarian_row'";
+								$librarian_result = oci_parse($connect, $librarian_sql);
+								oci_execute($librarian_result);
+								$librarian_data = oci_fetch_array($librarian_result);
+								echo $librarian_data['LIBRARIAN_NAME'];
+							?></td>
 							<td><?php echo $row['RESERVE_DATE']; ?></td>
 							<td><?php echo $row['RETURN_DATE']; ?></td>
 							<td><?php echo $row['DUE_DATE']; ?></td>
 							<td>
-								<a class="btn btn-primary" href="editborrow.php?BOOK_ID=<?php echo $row['BOOK_ID']; ?>">Edit</a>
-								<a class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this book?');" href="deleteborrow.php?BOOK_ID=<?php echo $row['BOOK_ID']; ?>">Delete</a>
+								<a class="btn btn-primary" href="editborrow.php?BOOK_ISBN=<?php echo $row['BOOK_ISBN']; ?>">Edit</a>
+								<a class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this book?');" href="deleteborrow.php?BOOK_ISBN=<?php echo $row['BOOK_ISBN']; ?>">Delete</a>
 							</td>
 						</tr>
 					<?php } ?>
@@ -72,7 +67,7 @@
 			</table>
 		</div>
 		<div class="position-relative mt-5">
-			<a href="addborrow.php" class="btn btn-primary position-absolute top-50 start-50 translate-middle">New Borrow</a>
+			<a href="addborrow.php" class="btn btn-primary position-absolute top-50 start-50 translate-middle">New Borrower</a>
 		</div>
 	</section>
 <?php include 'footer.php'; ?>
