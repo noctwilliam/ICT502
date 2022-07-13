f639c75863e3801703beefb191f12fff41414138
Harith Azmi <harith2312@gmail.com>
2022-07-14 00:19:07 +0800

    View Reports
    - rid navbar
    - position button add issue

--- viewReports.php
+++ viewReports.php
@@ -1,40 +1,17 @@
 <?php
-
+	include "header.php";
 	require "connect.php";
 	$query = "SELECT * FROM REPORTS";
 	$result = oci_parse($connect, $query);
 	oci_execute($result);
 ?>
 
-<?php include "header.php"; ?>
 	<section id="table">
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-	</li>
-	</li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Log Out</a>
-	</li>
-    </ul>
-  </div>
-</nav>
 		<div class="container">
 			<table class="table table-striped">
 				<thead>
 				<div style="text-align: center">
                 <p><b>REPORTS OF BORROWER</b></p>
-</div>
 					<tr>
 						<th>REPORT_ID</th>
 						<th>BOOK_ISBN</th>
@@ -53,9 +30,9 @@
 					<?php } ?>
 				</tbody>
 			</table>
-			<div style="text-align: center">
-			<a href="addReports.php" class="btn btn-warning">Add Issue</a>
-					</div>
+			<div class="position-absolute top-50 start-50 translate-middle">
+				<a href="addReports.php" class="btn btn-warning">Add Issue</a>
+			</div>
 		</div>
 	</section>
 <?php include 'footer.php'; ?>
