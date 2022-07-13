7b2ee424e436c5db530ec0271a7f6f3a0e02c119
Harith Azmi <harith2312@gmail.com>
2022-07-14 01:38:19 +0800

    corrected header

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <!-- This is the header -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,6 +11,12 @@
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/styles.css">
+	<!-- Font Icon -->
+	<link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+	<!-- Main css -->
+	<link rel="stylesheet" href="css/style.css">
 	<title>Book Library System</title>
 </head>
+
 <body>
