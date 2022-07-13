6795572b77ffa50c1282d213a8010ae91e7aa3ca
Harith Azmi <harith2312@gmail.com>
2022-07-14 01:37:34 +0800

    fix indentation

--- edituser.php
+++ edituser.php
@@ -36,42 +36,39 @@
 <?php include 'headeruser.php'; ?>
 
 <div class="main">
-<section class="signup">
-            <div class="container">
-                <div class="signup-content">
-                    <div class="signup-form">
-                        <h2 class="form-title">Edit User</h2>
-                        <form method="POST" class="register-form" id="register-form">
-                            <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="USER_EMAIL_input" id="email"  placeholder="Email" value="<?php echo $row['USER_EMAIL']; ?>"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="USER_NAME_input" id="name"  placeholder="Name" value="<?php echo $row['USER_NAME']; ?>"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="phone"><i class="zmdi zmdi-phone"></i></label>
-                                <input type="phone" name="USER_PHONENO_input" id="phone" placeholder="Phone Number" value="<?php echo $row['USER_PHONENO']; ?>"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="address"><i class="zmdi zmdi-home"></i></label>
-                                <input type="address" name="USER_ADDRESS_input" id="address" placeholder="Address" value="<?php echo $row['USER_ADDRESS']; ?>"/>
-                            </div>
-
-                            <div class="form-group my-4">
+	<section class="signup">
+		<div class="container">
+			<div class="signup-content">
+				<div class="signup-form">
+					<h2 class="form-title">Edit User</h2>
+					<form method="POST" class="register-form" id="register-form">
+						<div class="form-group">
+							<label for="email"><i class="zmdi zmdi-email"></i></label>
+							<input type="email" name="USER_EMAIL_input" id="email"  placeholder="Email" value="<?php echo $row['USER_EMAIL']; ?>"/>
+						</div>
+						<div class="form-group">
+							<label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+							<input type="text" name="USER_NAME_input" id="name"  placeholder="Name" value="<?php echo $row['USER_NAME']; ?>"/>
+						</div>
+						<div class="form-group">
+							<label for="phone"><i class="zmdi zmdi-phone"></i></label>
+							<input type="phone" name="USER_PHONENO_input" id="phone" placeholder="Phone Number" value="<?php echo $row['USER_PHONENO']; ?>"/>
+						</div>
+						<div class="form-group">
+							<label for="address"><i class="zmdi zmdi-home"></i></label>
+							<input type="address" name="USER_ADDRESS_input" id="address" placeholder="Address" value="<?php echo $row['USER_ADDRESS']; ?>"/>
+						</div>
+						<div class="form-group my-4">
 							<input type="submit" class="btn btn-primary" name="update" value="Update">
 							<a href="indexuser.php" class="btn btn-danger">Cancel</a>
-						    </div> 
-                          
-                            
-                        </form>
-                    </div>
-                    <div class="signup-image">
-                        <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
-                    </div>
-                </div>
-            </div>
-        </section>
+						</div>
+					</form>
+				</div>
+				<div class="signup-image">
+					<figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
 <?php include 'footer.php'; ?>
